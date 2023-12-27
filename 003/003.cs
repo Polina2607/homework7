5b7ffa9ca02615b6ba1ffa6ab715beba0d933552
Polina <polya_vi_13@mail.ru>
2023-12-27 20:17:18 +0300

    Исправила аргумент в 3 задаче

--- 003/003.cs
+++ 003/003.cs
@@ -4,7 +4,7 @@
 int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 printArrayRevers(array, array.Length-1);
 
-void printArrayRevers(array, int index)
+void printArrayRevers(int [] array, int index)
 {
     if(index<0)
     {
